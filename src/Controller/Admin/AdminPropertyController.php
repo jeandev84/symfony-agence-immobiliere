14b2b044ed328491a93f7d435651634b543d4dd8
Jean-Claude <jeanyao@ymail.com>
2019-09-26 21:17:48 +0300

    CRUD des biens immobilies

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,166 @@
+<?php
+namespace App\Controller\Admin;
+
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+/**
+ * Class AdminPropertyController
+ * @package App\Controller\Admin
+ */
+class AdminPropertyController extends AbstractController
+{
+     /**
+       * @var PropertyRepository
+      */
+      private $repository;
+
+      /**
+       * @var ObjectManager
+      */
+      private $em;
+
+
+    /**
+     * AdminPropertyController constructor.
+     * @param PropertyRepository $repository
+     * @param ObjectManager $em
+     */
+      public function __construct(PropertyRepository $repository, ObjectManager $em)
+      {
+          $this->repository = $repository;
+          $this->em = $em;
+      }
+
+
+      /**
+       * @Route("/admin", name="admin.property.index")
+       * @return \Symfony\Component\HttpFoundation\Response
+      */
+      public function index()
+      {
+          # recuperation de toutes les biens
+          $properties = $this->repository->findAll();
+          return $this->render('admin/property/index.html.twig', compact('properties'));
+      }
+
+
+    /**
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+      public function new(Request $request)
+      {
+          $property = new Property();
+
+          # On cree notre formulaire ( Formulaire )
+          $form = $this->createForm(PropertyType::class, $property);
+
+
+          # On demande a notre formulaire de gerer la requette
+          $form->handleRequest($request);
+
+
+          # On verifie si le formulaire a ete envoye et si les donnees sont valides
+          if($form->isSubmitted() && $form->isValid())
+          {
+              # persite sur notre nouvelle entity
+              $this->em->persist($property);
+
+              # on va sauvegarder les donnees
+              $this->em->flush();
+
+              # Ajouter un flash
+              $this->addFlash('success', 'Bien cree avec succes');
+
+              # redirige l'utilisateur vers la route admin.property.index
+              return $this->redirectToRoute('admin.property.index');
+          }
+
+          # Rendre la vue
+          return $this->render('admin/property/new.html.twig', [
+              'property' => $property,
+              'form' => $form->createView()
+          ]);
+
+      }
+
+
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param Property $property
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * createForm peut prendre en parametre la classe Type et les donnees(Entity ou array)
+     */
+      public function edit(Property $property, Request $request)
+      {
+           # On cree notre formulaire ( Formulaire )
+           $form = $this->createForm(PropertyType::class, $property);
+
+           # On demande a notre formulaire de gerer la requette
+           $form->handleRequest($request);
+
+
+           # On verifie si le formulaire a ete envoye et si les donnees sont valides
+           if($form->isSubmitted() && $form->isValid())
+           {
+                 # on va sauvegarder les donnees
+                 $this->em->flush();
+
+                # Ajouter un flash
+                $this->addFlash('success', 'Bien modifie avec succes');
+
+
+               # redirige l'utilisateur vers la route admin.property.index
+                 return $this->redirectToRoute('admin.property.index');
+           }
+
+
+           # retourne une reponse et les differents parametres
+           return $this->render('admin/property/edit.html.twig', [
+               'property' => $property,
+               'form' => $form->createView()
+           ]);
+      }
+
+
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
+     * @param Property $property
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+      public function delete(Property $property, Request $request)
+      {
+          # On verifie si le token est valide
+          if($this->isCsrfTokenValid('delete'. $property->getId(), $request->get('_token')))
+          {
+              # On retire l'objet qu'on veut
+              $this->em->remove($property);
+
+              # On sauvegarde les changements
+              $this->em->flush();
+
+
+              # Ajouter un flash
+              $this->addFlash('success', 'Bien supprime avec succes');
+
+
+              /* return new Response('Suppression'); */
+          }
+
+          # Redirection vers la page qui permet de lister les biens
+          return $this->redirectToRoute('admin.property.index');
+      }
+}
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+
+    /**
+     * Permet de contruire le formulaire
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat', ChoiceType::class, [ // select field
+                'choices' =>  $this->getChoices()
+            ])
+            ->add('city', null, [
+                'label' => 'Ville'  // redefinir le label
+            ])
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold');
+    }
+
+    /**
+     * Permet de definir les options du formulaire de facon global
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms' // ajout de system de traduction
+        ]);
+    }
+
+
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach($choices as $k => $v)
+        {
+            # inversion de cle $v comme cle et $k comme valeur
+           $output[$v] = $k;
+        }
+        return $output;
+    }
+
+    /*
+    private function formBuilding(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat')
+            ->add('city', null, [
+                'label' => 'Ville'
+            ])
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold');
+            // ->add('created_at'); En commentant cette ligne alors elle disparaitra
+    }
+    */
+}
